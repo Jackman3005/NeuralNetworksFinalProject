5324ea1ec2a8b958c179d48c1ceb0bfba616f5d6
Brett <brett.israelsen@gmail.com>
2015-05-04 11:14:05 -0600

    added more plots, also added network that writes out test data, parsed that file into .csv

--- plotResults.jl
+++ plotResults.jl
@@ -11,10 +11,14 @@ srtIter = sort(collect(keys(data["statistics"]["accuracy-accuracy"])))
 acc = []
 err = []
 epochs = []
+lr = []
+mom = []
 
 for i in srtIter
     acc = cat(1,acc,data["statistics"]["accuracy-accuracy"][i])
     err = cat(1,err,data["statistics"]["accuracy-error"][i])
+    mom = cat(1,mom,data["statistics"]["momentum"][i])
+    lr = cat(1,lr,data["statistics"]["learning-rate"][i])
     epochs = cat(1,epochs,int(i))
 end
 
@@ -22,8 +26,10 @@ end
 epochs = epochs.*100 ./ 150000
 
 #Each has two identical entries at the beginning, this removes the first
-shift!(acc)
-shift!(err)
+#shift!(acc)
+#shift!(err)
+#shift!(mom)
+#shift!(lr)
 
 plot(epochs,acc)
 title("Accuracy vs. Epoch")
@@ -34,3 +40,13 @@ plot(epochs,err)
 title("Error vs. Epoch")
 xlabel("Epochs")
 ylabel("Percent")
+
+plot(epochs,mom)
+title("Momentum vs. Epoch")
+xlabel("Epochs")
+ylabel("Momentum")
+
+plot(epochs,lr)
+title("Learning Rate vs. Epoch")
+xlabel("Epochs")
+ylabel("Learning Rate")
--- trainNet_parallel.jl
+++ trainNet_parallel.jl
@@ -13,12 +13,12 @@ function parse_commandline()
         "--numBLASThreads", "-B"
             help = "The number of BLAS threads to use"
             arg_type = Int
-            default = 1
+            default = 4
 
         "--numOMPThreads", "-O"
             help = "another option with an argument"
             arg_type = Int
-            default = 1
+            default = 4
 
         "--useCUDA", "-C"
             help = "A flag for whether CUDA should be used"
@@ -53,16 +53,16 @@ println(parsed_args)
 if parsed_args["useCUDA"]
   #We are going to use CUDA
   ENV["MOCHA_USE_CUDA"] = "true"
-else
-  #Don't use GPU
-  if !parsed_args["dontUseNativeExt"]
-    #this won't run by default
-    ENV["MOCHA_USE_NATIVE_EXT"] = "true"
-  end
-  #
-  ENV["OMP_NUM_THREADS"] = parsed_args["numOMPThreads"]
-  blas_set_num_threads(parsed_args["numBLASThreads"])
 end
+
+if !parsed_args["dontUseNativeExt"]
+#this won't run by default
+ENV["MOCHA_USE_NATIVE_EXT"] = "true"
+end
+#
+ENV["OMP_NUM_THREADS"] = parsed_args["numOMPThreads"]
+blas_set_num_threads(parsed_args["numBLASThreads"])
+
 
 base_path = parsed_args["basePath"]
 train_path = parsed_args["trainFile"]
@@ -87,14 +87,14 @@ fc1_layer   = InnerProductLayer(name="fc1", output_dim=prod(IR[1:2]), neuron=Neu
 #Convolution layer needs 4D tensor so we need to reshape outputs from InnerProductLayer (the fourth dimension is implicit)
 reshape_layer = ReshapeLayer(shape=IR,bottoms=[:fc1], tops=[:rs1])
 
-conv1_layer = ConvolutionLayer(name="conv1", n_filter=100, kernel=(5,5), pad=(2,2),
+conv1_layer = ConvolutionLayer(name="conv1", n_filter=64, kernel=(5,5), pad=(2,2),
     stride=(2,2), filter_init=XavierInitializer(), bottoms=[:rs1], tops=[:conv1])
 pool1_layer = PoolingLayer(name="pool1", kernel=(3,3), stride=(2,2), neuron=Neurons.ReLU(),
     bottoms=[:conv1], tops=[:pool1])
 norm1_layer = LRNLayer(name="norm1", kernel=3, scale=5e-5, power=0.75, mode=LRNMode.AcrossChannel(),
     bottoms=[:pool1], tops=[:norm1])
 
-conv2_layer = ConvolutionLayer(name="conv2", n_filter=50, kernel=(5,5), pad=(2,2),
+conv2_layer = ConvolutionLayer(name="conv2", n_filter=32, kernel=(5,5), pad=(2,2),
     stride=(2,2), filter_init=XavierInitializer(), bottoms=[:norm1], tops=[:conv2], neuron=Neurons.ReLU())
 pool2_layer = PoolingLayer(name="pool2", kernel=(3,3), stride=(2,2), pooling=Pooling.Mean(),
     bottoms=[:conv2], tops=[:pool2])
@@ -102,18 +102,23 @@ norm2_layer = LRNLayer(name="norm2", kernel=3, scale=5e-5, power=0.75, mode=LRNM
     bottoms=[:pool2], tops=[:norm2])
 
 ip1_layer   = InnerProductLayer(name="ip1", output_dim=121, weight_init=XavierInitializer(),
-    bottoms=[:norm2], tops=[:ip1])
+    bottoms=[:pool2], tops=[:ip1])
 
 loss_layer  = SoftmaxLossLayer(name="softmax", bottoms=[:ip1, :label])
 acc_layer   = AccuracyLayer(name="accuracy", bottoms=[:ip1, :label],report_error=true)
 
-common_layers = [fc1_layer,reshape_layer, conv1_layer, pool1_layer, norm1_layer, conv2_layer, pool2_layer, norm2_layer, ip1_layer]
+common_layers = [fc1_layer,reshape_layer, conv1_layer, pool1_layer, norm1_layer, conv2_layer, pool2_layer, ip1_layer]
 
 # setup dropout for the different layers
 # we use 10% dropout on the inputs and 50% dropout in the hidden layers
 # as these values were previously found to be good defaults
 drop_input  = DropoutLayer(name="drop_in", bottoms=[:data], ratio=0.1)
+drop_resh1 = DropoutLayer(name="drop_r1", bottoms=[:rs1], ratio=0.5)
+drop_conv1 = DropoutLayer(name="drop_c1", bottoms=[:conv1], ratio=0.5)
+drop_pool1 = DropoutLayer(name="drop_p1", bottoms=[:pool1], ratio=0.5)
 drop_norm1 = DropoutLayer(name="drop_n1", bottoms=[:norm1], ratio=0.5)
+drop_conv2 = DropoutLayer(name="drop_c2", bottoms=[:conv2], ratio=0.5)
+drop_pool2 = DropoutLayer(name="drop_p2", bottoms=[:pool2], ratio=0.5)
 drop_norm2 = DropoutLayer(name="drop_n2", bottoms=[:norm2], ratio=0.5)
 drop_ip1 = DropoutLayer(name="drop_ip1", bottoms=[:ip1], ratio=0.5)
 
@@ -124,8 +129,8 @@ else
 end
 init(backend)
 
-drop_layers = [drop_norm1, drop_norm2, drop_input, drop_ip1]
-
+drop_layers = [drop_resh1, drop_conv1, drop_conv2, drop_ip1]
+#drop_layers = []
 # put training net together, note that the correct ordering will automatically be established by the constructor
 net = Net("NDSB_train", backend, [data_layer, common_layers..., drop_layers..., loss_layer])
 
@@ -134,9 +139,8 @@ num_iters = 5000
 num_epocs = 1000
 
 params = SolverParameters(max_iter=num_iters*num_epocs, regu_coef=0.0,
-                          mom_policy=MomPolicy.Fixed(.9),
-                          #mom_policy=MomPolicy.Linear(0.5, 0.0008, num_iters, 0.9),
-                          lr_policy=LRPolicy.Step(.001,0.98,num_iters),
+                          mom_policy=MomPolicy.Linear(0.9, 0.98,num_iters,0.9),
+                          lr_policy=LRPolicy.Step(.01,0.98,num_iters),
                           load_from="$base_path/snapshots")
 solver = Nesterov(params)
 
