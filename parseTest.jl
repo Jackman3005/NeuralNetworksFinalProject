ec4c2e692dadfa32ec66b29b369b0f0a28406256
Brett <brett.israelsen@gmail.com>
2015-05-04 21:19:14 -0600

    updated output predictions

--- parseTest.jl
+++ parseTest.jl
@@ -3,8 +3,6 @@ using JLD
 
 cd("/home/brett/GitProjects/NeuralNetworksFinalProject/")
 
-fobj = open("testData.txt","w")
-
 data = load("snapshots/Outputs.jld")
 
-writecsv("testOutput.csv",data["ip1"][:,1:20500]')
+writecsv("testOutput.csv",data["ip1"][:,1:130401]')
